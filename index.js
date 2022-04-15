47ca1f35b623a2b22ca7369f1d316cbb56b61d4d
Mark Danielle Bien <mark.bien@outlook.com>
2022-04-15 13:51:25 +0800

    Add functionality

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+'use strict';
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 const Player = sign => {
     const _sign = sign;
     
@@ -7,9 +12,12 @@ const Player = sign => {
 }
 
 const gameBoard = (()=>{
-    const $ = document.querySelector.bind(document);
+    const cells = $$('.cell');
 
+    // creates the board array to hold values
     let _board = new Array(9);
+
+    // reference for winning values
     const _winningCombinations = [
         [0,1,2],
         [3,4,5],
@@ -21,26 +29,95 @@ const gameBoard = (()=>{
         [2,4,6]
     ];
 
+    // set the value for the chosen field
+    // input player sign to array and DOM
+    const setField = (num, marker) => {
+        const input = checkInput(marker);
 
-
-    const getField = num => _board[num];
-
-    const setField = (num, player) => {
-        const cellToFill = $(`.cell:nth-child(${num + 1})`);
-        cellToFill.classList.add(player);
-        cellToFill.dataset.value = player;
-        _board[num] = player;
+        // ensures that chosen field is not filled yet
+        if(input){
+            _board[num] = input;
+            const cellToFill = $(`.cell[data-index="${num}"]`);
+            cellToFill.classList.add(input);
+            cellToFill.dataset.value = input;            
+        }
     }
 
-    const checkArr = ()=> {
-        console.log(_board);
+    // checks that input is  only x and o
+    const checkInput = input => {
+        if(input === 'x' || input === 'o'){
+            return input;
+        }
     }
 
-    return { getField, setField, checkArr }
+    const resetGame = ()=> {
+        _board = [];
+        cells.forEach(cell => {
+            cell.classList.remove('x');
+            cell.classList.remove('o');
+        });
+        $('.announcement-container').classList.remove('show');
+    };
+
+    $('.controls > button').addEventListener('click', resetGame);
+
+
+    // Array.from(cells).forEach(cell => {
+    //     cell.addEventListener('click', ()=> {
+    //         setField(cell.dataset.index, currentPlayer);
+    //     });
+    // });
+
+    const getBoard = ()=> _board;
+
+    return { setField, resetGame, getBoard, _winningCombinations }
 })();
 
 
 
 const displayController = (()=>{
+    const player1 = Player('x');
+    const player2 = Player('o');
 
+    let activePlayer = true;
+    let currentClass;
+
+    function nextPlayer() {
+        activePlayer = !activePlayer;
+
+        if(activePlayer){
+            $('.cell-container').classList.toggle('o');
+            $('.cell-container').classList.toggle('x');
+        } else {
+            $('.cell-container').classList.toggle('x');
+            $('.cell-container').classList.toggle('o');
+        }
+    }
+
+    const markCell = (cell)=> {
+        currentClass = activePlayer ? player1.getSign() : player2.getSign();
+        cell.classList.add(currentClass);
+    }
+
+    const checkWin = (currentClass, cells) => {
+        return gameBoard._winningCombinations.some(combination => {
+            return combination.every(index => {
+                return cells[index].classList.contains(currentClass);
+            });
+        });
+    }
+
+    $$('.cell').forEach(cell=> {
+        cell.addEventListener('click', ()=> {
+            markCell(cell);
+            gameBoard.setField(cell.dataset.index, currentClass);
+            if(checkWin(currentClass, $$('.cell'))) {
+                $('.announcement-container').classList.add('show');
+                $('.announce-winner > h1 > span').textContent = currentClass.toUpperCase();
+            }
+            nextPlayer();
+        }, {once: true});
+    });
+
+    return {checkWin};
 })();
