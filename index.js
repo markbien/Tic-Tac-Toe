7369c729da773612bc6982be37f9779d1d4641cc
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-09-17 14:57:36 +0800

    Shorten the checkWinner function and re-enable checkIfAMarkAlreadyExists function

--- index.js
+++ index.js
@@ -1,153 +1,91 @@
-'use strict';
+"use strict";
 
-const gameBoard = (()=> {
-    const board = [
-        '1','2','3',
-        '4','5','6',
-        '7','8','9',
-    ];
+const gameBoard = (() => {
+  const board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    const showBoard = () => {
-        console.log(`[${board[0]}|${board[1]}|${board[2]}]`);
-        console.log(`[${board[3]}|${board[4]}|${board[5]}]`);
-        console.log(`[${board[6]}|${board[7]}|${board[8]}]\n`);
-    }
+  const showBoard = () => {
+    console.log(`[${board[0]}|${board[1]}|${board[2]}]`);
+    console.log(`[${board[3]}|${board[4]}|${board[5]}]`);
+    console.log(`[${board[6]}|${board[7]}|${board[8]}]\n`);
+  };
 
-    const getCurrentMarkerInACell = index => {
-        return board[index];
-    }
+  function setMarker(marker, place) {
+    place -= 1; // Since array starts from 0
+    board[place] = `${marker}`;
 
-    // function isValidMarker(marker) {
-    //     if (marker === "x" || marker === "o") {
-    //         return true;
-    //     }
-    //     return false;
-    // }
+    return checkWinner(marker) ? true : false;
+  }
 
-    // function isValidPlace(place) {
-    //     if (place >= 0 && place <= 8) return true;
-    //     return false;
-    // }
+  function checkIfAMarkAlreadyExists(place) {
+    place -= 1;
+    if (board[place] === "x" || board[place] === "o") return true;
+    else false;
+  }
 
-    // function isGoodToSetMarker(marker, place) {
-    //     if (!isValidMarker(marker) || !isValidPlace(place) || checkIfAMarkAlreadyExists(place) === true) return false;
-    //     return true;
-    // }
+  // function isBoardFull(){
+  //     for (let i = 0, len = board.length; i < len; i++) {
+  //         if (board[i] === "x" || board[i] === "o") continue;
+  //         else return false;
+  //     }
+  //     return true;
+  // }
 
-    function setMarker(marker, place) {
-        place -= 1; // Since array starts from 0
-        // if (isGoodToSetMarker(marker, place) == true) {
-        //     board[place] = `${marker}`;
-        //     return true;
-        // // }
-        // else return false;
-        board[place] = `${marker}`;
-    }
+  function checkWinner(marker) {
+    const combinations = winningCombinations.showWinningCombinations();
+    const winningInput = `${marker}${marker}${marker}`;
 
-    // function checkIfAMarkAlreadyExists(place){
-    //     if (board[place] === "x" || board[place] === "o") return true;
-    //     else false;
-    // }
+    const isThereAWinner = combinations.some((com) => {
+      let userInput = `${board[com[0]]}${board[com[1]]}${board[com[2]]}`;
+      if (winningInput === userInput) {
+        return true;
+      }
+    });
 
-    // function isBoardFull(){
-    //     for (let i = 0, len = board.length; i < len; i++) {
-    //         if (board[i] === "x" || board[i] === "o") continue;
-    //         else return false;
-    //     }
-    //     return true;
-    // }
+    return isThereAWinner ? true : false;
+  }
 
-    function checkWinner(marker){
-        // [x,o,o,x,_,_,x,_,_]
-        // [0,1,2,3,4,5,6,7,8]
-        const combinations = winningCombinations.showWinningCombinations();
-        let winner = false;
-        const winningInput = `${marker}${marker}${marker}`;
-
-        // Loop each winning combination
-        for (let i = 0, combinationsArrLen = combinations.length; i < combinationsArrLen; i++) {
-            let str = `${board[combinations[i][0]]}${board[combinations[i][1]]}${board[combinations[i][2]]}`;
-            if (str === winningInput) {
-                winner = true;
-                break;
-            }
-        }
-        return winner;
-    }
-
-    return { showBoard, setMarker, checkWinner, getCurrentMarkerInACell };
+  return { showBoard, setMarker, checkIfAMarkAlreadyExists };
 })();
 
-const winningCombinations = (()=> {
-    const winningCombinationsArray = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
-    ];
+const winningCombinations = (() => {
+  const winningCombinationsArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-    const showWinningCombinations = ()=> winningCombinationsArray;
+  const showWinningCombinations = () => winningCombinationsArray;
 
-    return {showWinningCombinations};
+  return { showWinningCombinations };
 })();
 
-const gameHandler = (()=> {
-    function init(){
-        const player1 = createPlayer("Player 1", "x");
-        // const player2 = createPlayer(askForName("Player 2"), "o");
+const gameHandler = (() => {
+  function init() {
+    gameBoard.showBoard();
 
-        gameBoard.showBoard();
+    gameBoard.setMarker("x", 1);
 
-        gameBoard.setMarker(player1.getMarker(), 1);
+    gameBoard.showBoard();
+  }
 
-        gameBoard.showBoard();
+  function askForInput(marker) {
+    const index = prompt(`Player ${marker}, enter a digit to place marker: `);
+    return index;
+  }
 
-        gameBoard.setMarker(player1.getMarker(), 4);
-
-        gameBoard.showBoard();
-
-        gameBoard.setMarker(player1.getMarker(), 7);
-
-        gameBoard.showBoard();
-
-        console.log(gameBoard.checkWinner(player1.getMarker()));
-    }
-
-    return { init };
+  return { init };
 })();
 
-function createPlayer(name, marker){
-    const getName = ()=> name;
-    const getMarker = ()=> marker;
+// function createPlayer(name, marker){
+//     const getName = ()=> name;
+//     const getMarker = ()=> marker;
 
-    return { getName, getMarker };
-}
+//     return { getName, getMarker };
+// }
 
-// gameBoard.setMarker("o", 0);
-// // gameBoard.showBoard();
-
-// gameBoard.setMarker("o", 1);
-// // gameBoard.showBoard();
-
-// gameBoard.setMarker("o", 2);
-// // gameBoard.showBoard();
-
-// gameBoard.setMarker("x", 2);
-// // gameBoard.showBoard();
-
-// gameBoard.setMarker("x", 3);
-// // gameBoard.showBoard();
-
-// gameBoard.setMarker("x", 6);
-// gameBoard.showBoard();
-
-// console.log(gameBoard.checkWinner("o"));
-
-// // console.log(gameBoard.isBoardFull());
-
-gameHandler.init(); 
-// console.log(gameBoard.isBoardFull());
+gameHandler.init();
